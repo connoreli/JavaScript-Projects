34172a60b5966121bb4f45e13f0bf60c8a19343b
Connor Eliason <connor.eliason@gmail.com>
2022-02-22 19:35:52 -0700

    js

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, world! ") // pop-up alert
+
+var connor = "my name "; //setting the variable connor equal to a string
+document.write(connor); //writing our variable on the page
+
+var haley = "Haley said, \"Don\'t tell Dad please!\" " + "Connor replied \"Eat my pants, loser!\"" // concatenating two strings
+document.write(haley) //printing the variable haley
+
+var family = "Eliason", dad = "Todd", mom = "Alison", oldestBrother = "Cody", olderBrother = "Dallin", olderSister = "Haley", me = "Connor" //listing multiple variables in one line
+olderBrother.fontcolor('green') //making Dallin's name green
+document.write(olderBrother) //printing the olderBrother string, dallin
+
+document.write(3 + 3) // statement containing expression
